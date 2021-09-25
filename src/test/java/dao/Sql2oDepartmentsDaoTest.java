aab45d522d4ad6e6b7a39616276dc92bc656d9b8
Esther-Moki <esther.moki@student.moringaschool.com>
2021-09-25 09:32:54 +0300

    generated Dao test for departments

--- src/test/java/dao/Sql2oDepartmentsDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentsDaoTest.java
@@ -1,5 +1,6 @@
 package dao;
 
+import models.Departments;
 import models.Users;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,14 +12,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Sql2oDepartmentsDaoTest {
     private Connection conn;
-    private Sql2oNewsDao newsDao;
-    private Sql2oDepartmentsDao departmentsDao;
+    public Sql2oNewsDao newsDao;
+     public Sql2oUsersDao usersDao;
+    public Sql2oDepartmentsDao departmentsDao;
 
     @BeforeEach
     public void setUp() throws Exception {
         String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "moringa", "1234");
         newsDao = new Sql2oNewsDao(sql2o);
+        usersDao = new Sql2oUsersDao(sql2o);
         departmentsDao = new Sql2oDepartmentsDao(sql2o);
         conn = sql2o.open();
     }
@@ -29,9 +32,21 @@ class Sql2oDepartmentsDaoTest {
     }
 
     @Test
-    void addingDepartmentsSetsId()  throws Exception {
-        Users testUsers = setUpUsers();
-        assertEquals(1, testUsers.getId());
+    public void addingDepartmentsSetId()  throws Exception {
+        Departments testDepartments = setUpDepartments();
+        assertEquals(1,  testDepartments.getId());
     }
-    
+
+//    @Test
+//      public void getAll() throws Exception {
+//         Departments departments1 =  setUpDepartments();
+//         Departments departments2 =  setUpDepartments();
+//        assertEquals(2,     Departments.getAll().size());
+//      }
+//
+    private Departments setUpDepartments() {
+        return null;
+    }
+
+
 }
